c46267c58803d275761ec7c4ff05a6cf716b7a0c
Ma. de los Angeles Lira Flores <angelitaflor2018@gmail.com>
2019-12-01 23:47:17 -0600

    Feat: se termina el desarrollo del ciclo for y se evalua si se cumple o no

--- consultadedatos.py
+++ consultadedatos.py
@@ -10,3 +10,18 @@ class BusquedaSecuencial:
                     break
         return encontrado
         #encontrado = False
+
+        # for i in lista:
+
+        #    if i == clave:
+        #        encontrado = True
+         #       break
+        # return encontrado
+
+# def main():
+ #   A = [['aaron', 7676798563], ['abel', 7676798564], ['abelardo', 7676798565], ['abigail', 7676798566], ['abraham', 7676798567]]
+  #  b = BusquedaSecuencial()
+    # print(b.busqueda(A, 7676798563))
+
+# if __name__ == "__main__":
+  #   main()
